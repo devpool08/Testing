323c18b6059d55472d8a26d47067830e06a9c90c
Debadatta Pujhari <debadatta_pujhari@epam.com>
2025-02-22 11:04:19 +0530

    Add deserialization and serialization tests for REST API responses

---
+++ src/test/java/org/example/restAssured/c_serialization/TestDeserialization.java
@@ -0,0 +1,53 @@
+package org.example.rendom_test.Lam;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+class Product {
+    private String id;
+    private String name;
+
+    @JsonProperty("data")
+    private ProductData data;
+
+    @Data
+    public static class ProductData {
+        private int year;
+        private double price;
+
+        @JsonProperty("CPU model")
+        private String cpuModel;
+
+        @JsonProperty("Hard disk size")
+        private String hardDiskSize;
+    }
+}
+
+public class TestDeserialization {
+    private static final String REQUEST_PATH = "/7";
+    private ObjectMapper objectMapper;
+
+    @BeforeClass
+    public void setup() {
+        baseURI = "https://api.restful-api.dev/";
+        basePath = "objects/";
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    public void deserializeJsonResponse() throws JsonProcessingException {
+        String response = given().get(REQUEST_PATH).then().extract().asString();
+        System.out.println(response);
+        Product p = objectMapper.readValue(response, Product.class);
+        System.out.println(p.toString());
+    }
+}
---
+++ src/test/java/org/example/restAssured/c_serialization/TestSerialization.java
@@ -0,0 +1,48 @@
+package org.example.rendom_test.Lam;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Entity {
+    private String name;
+    private String job;
+}
+
+public class TestSerialization {
+    private static final String REQUEST_PATH = "/users";
+    private static Entity entity;
+    private ObjectMapper objectMapper;
+
+    @BeforeClass
+    public void setup() {
+        baseURI = "https://reqres.in";
+        basePath = "/api/";
+        entity = new Entity();
+        objectMapper = new ObjectMapper();
+    }
+
+
+    @Test
+    public void sendSerializedRequest() throws JsonProcessingException {
+        entity = new Entity("john", "test");
+        String jsonSchema = objectMapper.writeValueAsString(entity);
+        given().
+                body(jsonSchema).
+                when().
+                post(REQUEST_PATH).
+                then().
+                statusCode(HttpStatus.SC_CREATED);
+    }
+
+}
